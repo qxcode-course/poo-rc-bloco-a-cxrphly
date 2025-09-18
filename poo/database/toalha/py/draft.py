0380db8b10253c7913e15a35916ef8d4a823df9a
halyson <halyson.php@gmail.com>
2025-09-18 15:48:41 -0300

    Instalando tko maquina local

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,15 +1,55 @@
 class Towel:
-    def __init__(self, color:str ,size:str):
-        self.color:str = color
-        self.size:str = size
-        self.wetness:int = 0
-    def __str__(self)->str:
-        return f"{self.color}:{self.size}:{self.wetness}"
-colorInput = input()
-sizeInput = input()
+    def __init__ (self, color:str, size:str):
+        self.color = color
+        self.size = size
+        self.wetness = 0
+        
+    def getMaxWetness(self) -> int: 
+        match self.size:
+            case "P":
+                return 10
+            case "M":
+                return 20
+            case "G":
+                return 30
+            case _:
+                return 0
+    
+    def dry(self, amount:int)-> None:
+        self.wetness += amount
+        if self.wetness > self.getMaxWetness():
+            print("toalha encharcada")
+            self.wetness = self.getMaxWetness()
 
-minha:Towel = Towel(colorInput,sizeInput)
+    def wringOut(self) -> None:
+        self.wetness = 0
 
-print(f"{minha}")
+    def isDry(self) -> bool:
+        return self.wetness == 0
+    
+    def show(self) -> None:
+        print(self)
+        
+    def __str__(self) -> str:
+        return f"{self.color} {self.size} {self.wetness}"
 
 
+towel = Towel("Azul", "P")
+towel.show()
+towel.dry(5)
+towel.show()  # Azul P 5
+print(towel.isDry()) # False
+towel.dry(5)
+towel.show()  # Azul P 10
+towel.dry(5) # msg: toalha encharcada
+towel.show()  # Azul P 10
+
+towel.wringOut()
+towel.show()  # Azul P 0
+
+towel = Towel("Verde", "G")
+print(towel.isDry()) # True
+towel.dry(30)
+towel.show()  # Verde G 30
+print(towel.isDry()) # False
+towel.dry(1)  # msg: toalha encharcada
