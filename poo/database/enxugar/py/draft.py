987343e873c4ada3bc400265c3dbb4a3a31a92ba
halyson <halyson.php@gmail.com>
2025-09-23 14:57:50 -0300

    Towel task: print line fix

--- poo/database/enxugar/py/draft.py
+++ poo/database/enxugar/py/draft.py
@@ -1,9 +1,9 @@
 class Towel:
-    def __init__ (self, color:str = "", size:str = ""):
+    def __init__ (self, color: str = "", size: str = ""):
         self.color = color
         self.size = size
         self.wetness = 0
-        
+            
     def getMaxWetness(self) -> int: 
         match self.size:
             case "P":
@@ -14,41 +14,36 @@ class Towel:
                 return 30
             case _:
                 return 0
-    
-    def dry(self, amount:int)-> None:
+
+    def dry (self, amount: int) -> None:
         self.wetness += amount
-        if self.wetness >= self.getMaxWetness(): # verifica a umidade, se maior que o maximo, imprime encharcada e atribui a umidade o valor maximo
+        if self.wetness >= self.getMaxWetness():
             print("toalha encharcada")
             self.wetness = self.getMaxWetness()
 
     def wringOut(self) -> None:
         self.wetness = 0
+
     def isDry(self) -> bool:
-            if self.wetness == 0:
-                return True
-            else:
-                return False
-    def show(self) -> None:
-        print(f"Cor: {self.color} Tamanho: {self.size} Umidade: {self.wetness}")
+        return self.wetness == 0
         
     def __str__(self) -> str:
-        return f"Cor: {self.color} Tamanho: {self.size} Umidade: {self.wetness}"
-
+        return f"Cor: {self.color}, Tamanho: {self.size}, Umidade: {self.wetness}"
 
 def main():
-    towel:Towel = Towel(size="", color="")
-    while True:
-        line = input()
-        arg : list[str] = line.split(" ")
+    towel: Towel = Towel("","")
 
+    while True:
+        line: str = input()
+        arg : list[str] = line.split(" ")
+        print("$" + line)
         if arg[0] == "end":
             break
         elif arg[0] == "criar":
-            towel: Towel = Towel(color=arg[1], size=arg[2])
+            towel = Towel(color=arg[1], size=arg[2])
         elif arg[0] == "mostrar":
-            towel.show()
+            print(towel)
         elif arg[0] == "seca":
-            towel.isDry()
             print("sim" if towel.isDry() else "nao")
         elif arg[0] == "enxugar":
             amount:int = int(arg[1])
